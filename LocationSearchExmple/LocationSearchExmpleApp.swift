a0522c271e54bab8228af9f9fe339d712cd71dd3
katafuchi <katafuchix@gmail.com>
2023-01-08 20:27:33 +0900

    Initial Commit

---
+++ LocationSearchExmple/LocationSearchExmpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchExmpleApp.swift
+//  LocationSearchExmple
+//
+//  Created by cano on 2023/01/08.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearchExmpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
